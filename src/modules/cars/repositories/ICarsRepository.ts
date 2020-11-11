77aa448ac1ba0121183b7408bdd70178d30f7cc1
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-11-11 16:25:22 -0300

    chore: Change name of the interface to correct one

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -1,7 +1,7 @@
 import ICreateCarDTO from '@modules/cars/dtos/ICreateCarDTO';
 import Car from '../infra/typeorm/entities/Car';
 
-export default interface IUsersRepository {
+export default interface ICarsRepository {
   create(carData: ICreateCarDTO): Promise<Car>;
 
   findByName(name: string): Promise<Car | undefined>;
