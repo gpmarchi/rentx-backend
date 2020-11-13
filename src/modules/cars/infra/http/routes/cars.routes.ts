397607b7874ed4a487dba6a5ed41024b332abcb3
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-11-13 12:58:04 -0300

    fix: Turn into auth protected routes

--- src/modules/cars/infra/http/routes/cars.routes.ts
+++ src/modules/cars/infra/http/routes/cars.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import authenticate from '@modules/users/infra/http/middlewares/authenticate';
 import CarsController from '../controllers/CarsController';
-
 import carCreateValidator from '../validators/CarCreate';
 
 const carsRouter = Router();
 const carsController = new CarsController();
+
+carsRouter.use(authenticate);
 
 carsRouter.post('/', carCreateValidator, carsController.create);
 
--- src/modules/cars/infra/http/routes/specifications.routes.ts
+++ src/modules/cars/infra/http/routes/specifications.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import authenticate from '@modules/users/infra/http/middlewares/authenticate';
 import SpecificationsController from '../controllers/SpecificationsController';
-
 import specificationCreateValidator from '../validators/SpecificationCreate';
 
 const specificationsRouter = Router();
 const specificationsController = new SpecificationsController();
+
+specificationsRouter.use(authenticate);
 
 specificationsRouter.post(
   '/',
